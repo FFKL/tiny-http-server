8a1bd179b3a40086d2c44451e35326d2ade09601
dmitrii <ffklffkl@gmail.com>
2021-09-22 18:54:16 +0300

    add adder form to example

--- cgi-src/adder.c
+++ cgi-src/adder.c
@@ -6,24 +6,26 @@
 
 int main(void)
 {
-  char *buf, *p;
+  char *buf;
   char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
   int n1 = 0, n2 = 0;
   /* Extract the two arguments */
   if ((buf = getenv("QUERY_STRING")) != NULL)
   {
-    p = strchr(buf, '&');
-    *p = '\0';
-    strcpy(arg1, buf);
-    strcpy(arg2, p + 1);
-    n1 = atoi(arg1);
-    n2 = atoi(arg2);
+    char *arg1p = strstr(buf, "num1=");
+    char *arg2p = strstr(buf, "num2=");
+    if (arg1p != NULL && arg2p != NULL)
+    {
+      strcpy(arg1, arg1p + 5);
+      strcpy(arg2, arg2p + 5);
+      n1 = atoi(arg1);
+      n2 = atoi(arg2);
+    }
   }
   /* Make the response body */
   sprintf(content, "Welcome to add.com: ");
   sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
-  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>",
-          content, n1, n2, n1 + n2);
+  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
   sprintf(content, "%sThanks for visiting!\r\n", content);
 
   /* Generate the HTTP response */
