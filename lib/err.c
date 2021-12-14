2e37631eac122b54e5663605f1f1db2a855e2713
dmitrii <ffklffkl@gmail.com>
2021-12-14 22:32:17 +0200

    fix errors exit code

--- lib/err.c
+++ lib/err.c
@@ -13,23 +13,23 @@
 void unix_error(char *msg) /* Unix-style error */
 {
     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
-    exit(0);
+    exit(1);
 }
 
 void posix_error(int code, char *msg) /* Posix-style error */
 {
     fprintf(stderr, "%s: %s\n", msg, strerror(code));
-    exit(0);
+    exit(1);
 }
 
 void dns_error(char *msg) /* DNS-style error */
 {
     fprintf(stderr, "%s: DNS error %d\n", msg, h_errno);
-    exit(0);
+    exit(1);
 }
 
 void app_error(char *msg) /* Application error */
 {
     fprintf(stderr, "%s\n", msg);
-    exit(0);
+    exit(1);
 }
